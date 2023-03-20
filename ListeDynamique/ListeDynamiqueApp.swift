6d85d9fe0330438c26b76ab971b5d0ff8242eb4c
Chikaodili Deng <mchikky@yahoo.com>
2023-03-20 10:30:09 +0100

    Initial Commit

---
+++ ListeDynamique/ListeDynamiqueApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListeDynamiqueApp.swift
+//  ListeDynamique
+//
+//  Created by Chikaodili Deng on 20/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ListeDynamiqueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
